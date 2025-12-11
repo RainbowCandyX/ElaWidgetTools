1b31e4faa2b1411551848bd793c6bfb23a2d7726
Rainbow <RainbowCandyX@users.noreply.github.com>
2025-12-11 14:07:53 +0400

    Fix HSV color parameters out of range error
    - Clamp HSV values to [0.0, 1.0] using qBound
    - Fix ElaColorPicker initialization and mouse handlers
    - Prevent QColor::setHsvF warnings

--- ElaWidgetTools/DeveloperComponents/ElaColorPicker.cpp
+++ ElaWidgetTools/DeveloperComponents/ElaColorPicker.cpp
@@ -3,6 +3,7 @@
 #include <QMouseEvent>
 #include <QPainter>
 #include <QPainterPath>
+#include <QtMath>
 #include <utility>
 
 #include "ElaTheme.h"
@@ -19,7 +20,9 @@ ElaColorPicker::ElaColorPicker(QWidget* parent)
         for (int x = 0; x < colorPickerImage.width(); x++)
         {
             QColor pixColor;
-            pixColor.setHsvF((qreal)1 / colorPickerPix.width() * x, 1 - (qreal)1 / (colorPickerPix.height() - 1) * y, 1);
+            qreal h = qBound(0.0, (qreal)x / colorPickerPix.width(), 1.0);
+            qreal s = qBound(0.0, 1 - (qreal)y / (colorPickerPix.height() - 1), 1.0);
+            pixColor.setHsvF(h, s, 1);
             pixColor = pixColor.toRgb();
             colorPickerImage.setPixelColor(x, y, pixColor);
         }
@@ -48,7 +51,9 @@ void ElaColorPicker::mousePressEvent(QMouseEvent* event)
     _selectedPoint = _adjustPointLimit(event->pos());
     QPointF colorPoint((_selectedPoint.x() - 3) / 254.0 * 359, (_selectedPoint.y() - 3) / 254.0 * 359);
     QColor pixColor;
-    pixColor.setHsvF((qreal)1 / _colorPickerImage.width() * colorPoint.x(), 1 - (qreal)1 / (_colorPickerImage.height() - 1) * colorPoint.y(), 1);
+    qreal h = qBound(0.0, colorPoint.x() / _colorPickerImage.width(), 1.0);
+    qreal s = qBound(0.0, 1 - colorPoint.y() / (_colorPickerImage.height() - 1), 1.0);
+    pixColor.setHsvF(h, s, 1);
     _selectedColor = pixColor.toRgb();
     Q_EMIT selectedColorChanged(_selectedColor);
     QWidget::mousePressEvent(event);
@@ -60,7 +65,9 @@ void ElaColorPicker::mouseReleaseEvent(QMouseEvent* event)
     _selectedPoint = _adjustPointLimit(event->pos());
     QPointF colorPoint((_selectedPoint.x() - 3) / 254.0 * 359, (_selectedPoint.y() - 3) / 254.0 * 359);
     QColor pixColor;
-    pixColor.setHsvF((qreal)1 / _colorPickerImage.width() * colorPoint.x(), 1 - (qreal)1 / (_colorPickerImage.height() - 1) * colorPoint.y(), 1);
+    qreal h = qBound(0.0, colorPoint.x() / _colorPickerImage.width(), 1.0);
+    qreal s = qBound(0.0, 1 - colorPoint.y() / (_colorPickerImage.height() - 1), 1.0);
+    pixColor.setHsvF(h, s, 1);
     _selectedColor = pixColor.toRgb();
     Q_EMIT selectedColorChanged(_selectedColor);
     QWidget::mouseReleaseEvent(event);
@@ -72,7 +79,9 @@ void ElaColorPicker::mouseMoveEvent(QMouseEvent* event)
     _selectedPoint = _adjustPointLimit(event->pos());
     QPointF colorPoint((_selectedPoint.x() - 3) / 254.0 * 359, (_selectedPoint.y() - 3) / 254.0 * 359);
     QColor pixColor;
-    pixColor.setHsvF((qreal)1 / _colorPickerImage.width() * colorPoint.x(), 1 - (qreal)1 / (_colorPickerImage.height() - 1) * colorPoint.y(), 1);
+    qreal h = qBound(0.0, colorPoint.x() / _colorPickerImage.width(), 1.0);
+    qreal s = qBound(0.0, 1 - colorPoint.y() / (_colorPickerImage.height() - 1), 1.0);
+    pixColor.setHsvF(h, s, 1);
     _selectedColor = pixColor.toRgb();
     Q_EMIT selectedColorChanged(_selectedColor);
     QWidget::mouseMoveEvent(event);
